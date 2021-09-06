a699d06af50a1b9fd3a2adcc47c1f24b08f6c21e
Baraker42 <hakl6@seznam.cz>
2021-09-06 09:57:01 +0200

    úprava v logice tvorby herních buněk a úprava stylování textu

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ var height =10;
 var width = 10;
 var resetBoxes = function () {
    var sketch = document.getElementById("container-sketch")
-   console.log(sketch)
     for (var i = 0; i < height; i++) {
+        var column = document.createElement("div");
+        column.setAttribute("class",("playground-column"+i))
+        console.log(i)
+        sketch.appendChild(column)
         for (var j = 0; j < width; j++) {
             var cell=document.createElement("div");
             cell.setAttribute("class","playground")
             var cudlik = document.createElement("button");
-            sketch.appendChild(cell)
+            column.appendChild(cell)
             cell.appendChild(cudlik)
 
         }
